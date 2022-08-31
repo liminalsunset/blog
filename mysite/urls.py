c4cb2d6e119c91cf3e5bcaa896a5d0f86fa125bc
dev-yoga <spin_shoppe@icloud.com>
2022-08-31 16:43:20 -0700

    url patterns

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+
+app_name = 'blog'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+        views.post_detail,
+        name='post_detail'),
 ]
