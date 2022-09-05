de167d95e220517d2c41c960fc74452904623003
dev-yoga <spin_shoppe@icloud.com>
2022-09-05 09:23:34 -0700

    comment submit works

--- blog/forms.py
+++ blog/forms.py
@@ -8,3 +8,8 @@ class EmailPostForm(forms.Form):
     to = forms.EmailField()
     comments = forms.CharField(required=False,
                                widget=forms.Textarea)
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
