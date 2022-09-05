42f3cec905457b4b62f7a16377ff535f4646cb0f
dev-yoga <spin_shoppe@icloud.com>
2022-09-05 10:47:16 -0700

    tags, tagging and tag search work

--- blog/views.py
+++ blog/views.py
@@ -5,24 +5,30 @@ from django.views.generic import ListView
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
-    paginator = Paginator(object_list, 3) # 3 posts in each page
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
+    paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
-        # If page is not an integer deliver the first page
         posts = paginator.page(1)
     except EmptyPage:
-        # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
     return render(request,
                  'blog/post/list.html',
                  {'page': page,
-                  'posts': posts})
+                  'posts': posts,
+                  'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -32,20 +38,15 @@ def post_detail(request, year, month, day, post):
                                    publish__month=month,
                                    publish__day=day)
 
-    # List of active comments for this post
     comments = post.comments.filter(active=True)
 
     new_comment = None
 
     if request.method == 'POST':
-        # A comment was posted
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
-            # Create Comment object but don't save to database yet
             new_comment = comment_form.save(commit=False)
-            # Assign the current post to the comment
             new_comment.post = post
-            # Save the comment to the database
             new_comment.save()
     else:
         comment_form = CommentForm()
